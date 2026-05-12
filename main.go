2027491acd7d7c723a4c55a0f235658958d8b420
Yustinia <yustinia@protonmail.com>
2026-05-12 22:09:59 +0800

    fetch wall links

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -16,8 +19,16 @@ type Settings struct {
 	Page       int    `json:"page"`
 }
 
+type Wallpaper struct {
+	Path string `json:"path"`
+}
+
+type Response struct {
+	Data []Wallpaper `json:"data"`
+}
+
 func main() {
-	baseURL := "https://wallhaven.cc/api/v1/search"
+	baseURL := "https://wallhaven.cc/api/v1/search?"
 	configData, err := os.ReadFile("configs/settings.json")
 	if err != nil {
 		panic(err)
@@ -27,5 +38,33 @@ func main() {
 	err = json.Unmarshal(configData, &settings)
 	if err != nil {
 		panic(err)
+	}
+
+	params := url.Values{}
+	params.Set("apikey", settings.API)
+	params.Set("categories", settings.Categories)
+	params.Set("purity", settings.Purity)
+	params.Set("sorting", settings.Sorting)
+	params.Set("order", settings.Order)
+	params.Set("atleast", settings.AtLeast)
+	params.Set("ratios", settings.Ratios)
+	params.Set("page", fmt.Sprintf("%d", settings.Page))
+
+	fullURL := baseURL + params.Encode()
+
+	resp, err := http.Get(fullURL)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	var result Response
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		panic(err)
+	}
+
+	for index, wall := range result.Data {
+		fmt.Printf("[%d]: %v\n", index, wall.Path)
 	}
 }
