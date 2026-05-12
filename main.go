afa12aa150d5763089724c1bfde9674f09a201c4
Yustinia <yustinia@protonmail.com>
2026-05-12 18:09:01 +0800

    add settings struct

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"os"
+)
+
+type Settings struct {
+	API        string `json:"api_key"`
+	Categories string `json:"categories"`
+	Purity     string `json:"purity"`
+	Sorting    string `json:"sorting"`
+	Order      string `json:"order"`
+	AtLeast    string `json:"atleast"`
+	Ratios     string `json:"ratios"`
+	Page       int    `json:"page"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	baseURL := "https://wallhaven.cc/api/v1/search"
+	configData, err := os.ReadFile("configs/settings.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var settings Settings
+	err = json.Unmarshal(configData, &settings)
+	if err != nil {
+		panic(err)
+	}
 }
